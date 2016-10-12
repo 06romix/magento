23eaab61ae8313a406de8d7f3d25600d276830e0
Mesija Ubuntu <mesijart@gmail.com>
2016-10-12 18:17:43 +0300

    0.1.4  -  add support 404 page

--- app/code/community/Plumrocket/FAQ/controllers/IndexController.php
+++ app/code/community/Plumrocket/FAQ/controllers/IndexController.php
@@ -1,59 +1,80 @@
 <?php
 class Plumrocket_FAQ_IndexController extends Mage_Core_Controller_Front_Action
 {
-    public function IndexAction()
-    {
-      $this->_initAction();
-      $this->renderLayout();
+
+  /**
+   * Show list of active post
+   */
+  public function indexAction()
+  {
+    $this->_initAction();
+    $this->renderLayout();
+  }
+
+  /**
+   * Show post by id
+   *
+   * If post empty or don't exists - show 404 page from CMS_INDEX_NoROUTE
+   *
+   * @return bool
+   */
+  public function viewAction()
+  {
+    $model = Mage::getModel('psfaq/post');
+    $post_id = (int)$this->getRequest()->getParam('post');
+    if ($model->load($post_id)->isEmpty()) {
+      $this->_forward('cms_index_noroute');
+      return false;
     }
 
-    public function ViewAction()
-    {
-      $this->_initAction();
-      $this->renderLayout();
-    }
+    Mage::register('psfaq_post', $model);
 
-    protected function _initAction()
-    {
-      $this->loadLayout();
-      /**
-       * @var $head Mage_Page_Block_Html_Head
-       */
-      $head = $this->getLayout()->getBlock('head');
-      $head->setTitle($this->__('FAQ'));
+    $this->_initAction();
+    $this->renderLayout();
+    return true;
+  }
 
-      /**
-       * @var $breadcrumbs Mage_Page_Block_Html_Breadcrumbs
-       */
-      $breadcrumbs = $this->getLayout()->getBlock('breadcrumbs');
+  protected function _initAction()
+  {
+    $this->loadLayout();
+    /**
+     * @var $head Mage_Page_Block_Html_Head
+     */
+    $head = $this->getLayout()->getBlock('head');
+    $head->setTitle($this->__('FAQ'));
 
+    /**
+     * @var $breadcrumbs Mage_Page_Block_Html_Breadcrumbs
+     */
+    $breadcrumbs = $this->getLayout()->getBlock('breadcrumbs');
+
+    $breadcrumbs->addCrumb(
+      'home',
+      [
+        'label' => $this->__('Home Page'),
+        'title' => $this->__('Home Page'),
+        'link'  => Mage::getBaseUrl(),
+      ]
+    );
+
+    $breadcrumbs->addCrumb(
+      'titleName',                            // class
+      [
+        'label' => $this->__('FAQ'),          // >text<
+        'title' => $this->__('FAQ'),          // <a title="">
+        'link'  => Mage::getUrl('psfaq/'),    // <a href="">
+      ]
+    );
+
+    $post_id = (int)$this->getRequest()->getParam('post');
+    if ($post_id) {
       $breadcrumbs->addCrumb(
-        'home',
+        'view',
         [
-          'label' => $this->__('Home Page'),
-          'title' => $this->__('Home Page'),
-          'link'  => Mage::getBaseUrl(),
+          'label' => $this->__('view'),
+          'title' => $this->__('view'),
         ]
       );
-
-      $breadcrumbs->addCrumb(
-        'titleName',                            // class
-        [
-          'label' => $this->__('FAQ'),          // >text<
-          'title' => $this->__('FAQ'),          // <a title="">
-          'link'  => Mage::getUrl('psfaq/'),    // <a href="">
-        ]
-      );
-
-      $post_id = (int)$this->getRequest()->getParam('post');
-      if ($post_id) {
-        $breadcrumbs->addCrumb(
-          'view',
-          [
-            'label' => $this->__('view'),
-            'title' => $this->__('view'),
-          ]
-        );
-      }
     }
+  }
 }
